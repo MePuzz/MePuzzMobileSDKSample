c500281b1b4a91e30b42dbea39700a7e7ed20af6
hanhns <hanhns88@gmail.com>
2020-04-05 11:59:47 +0700

    Add Mepuzz Mobile SDK Sample

---
+++ app/src/main/java/com/mepuzz/de/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mepuzz.de;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mepuzz.mobilesdk.MePuzzService;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MePuzzService.init("BXQ9JLy4nE", this);
+    }
+}
